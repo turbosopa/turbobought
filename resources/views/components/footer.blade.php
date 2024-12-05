4300b5fde0b5e9cf737a875526ef3cee78f3e504
turbosopa <deusopa29@gmail.com>
2024-12-05 13:04:14 +0100

    homeIAlgoMés

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="w-screen h-20 bg-gray-700 text-white text-center py-3 fixed bottom-0">
+  <div class="">
+    
+    <a href="http://multimedia.uvic.cat/" target="_blank">
+      Grau en Multimèdia, Aplicacions i Videojocs
+    </a>
+    <a href="https://www.uvic.cat/centre/fcte">(Facultat de Ciències, Tecnologia i Enginyeries)</a>.
+  </div>
+  <div class="">
+    Copyright - WebDev UVic © {{ now()->year }}
+  </div>
+
+</footer>
